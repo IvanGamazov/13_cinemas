db0f1cadc1038ed171798f33c38e698732224dc6
IvanGamazov <ivan.gamazov@gmail.com>
2016-10-19 22:08:46 +0300

    Используются map и filter, работа с параметрами  requests.get, чистые функции. Добавлен requirements.txt

--- cinemas.py
+++ cinemas.py
@@ -1,23 +1,29 @@
 import requests
-import os
 from urllib.request import urlretrieve
 from bs4 import BeautifulSoup
+from tempfile import NamedTemporaryFile
+
+PAGE_URL = 'http://www.afisha.ru/msk/schedule_cinema/'
+KINOPOISK_SEARCH_API = 'http://api.kinopoisk.cf/searchFilms'
 
 
 def fetch_afisha_page():
-    page = 'page.html'
-    urlretrieve('http://www.afisha.ru/msk/schedule_cinema/', page)
+    page = NamedTemporaryFile()
+    urlretrieve(PAGE_URL, page.name)
     return page
 
 
+def get_film_info_from_div(div):
+    cinemas_count = len(div.table.tbody.find_all('tr'))
+    block_divs = div.find_all('div')
+    name = get_cinema_caption(block_divs)
+    return {'name': name, 'cinemas_count': cinemas_count}
+
+
 def get_movies_from_afisha(divs):
-    movies = []
-    for div in divs:
-        if 'class' in div.attrs and 'object' in div.get('class'):
-            cinemas_count = len(div.table.tbody.find_all('tr'))
-            block_divs = div.find_all('div')
-            name = get_cinema_caption(block_divs)
-            movies.append({'name': name, 'cinemas_count': cinemas_count})
+    movie_divs = list(filter(lambda div: 'class' in div.attrs and
+                                         'object' in div.get('class'), divs))
+    movies = list(map(lambda div: get_film_info_from_div(div), movie_divs))
     return movies
 
 
@@ -28,20 +34,19 @@ def get_cinema_caption(div_tags):
             return cinema_div.h3.string
 
 
-def parse_afisha_list(raw_html):
-    if os.path.exists(raw_html):
-        with open(raw_html) as html_doc:
-            soup = BeautifulSoup(html_doc, 'html.parser')
-            div_tags = soup.find_all('div')
-        return div_tags
+def parse_afisha_list(raw_html_file):
+    soup = BeautifulSoup(raw_html_file.read(), 'html.parser')
+    div_tags = soup.find_all('div')
+    return div_tags
 
 
 def fetch_movie_info(movie_title):
-    response = requests.get('http://api.kinopoisk.cf/searchFilms?keyword=' + movie_title).json()
+    payload = {'keyword': movie_title}
+    response = requests.get(KINOPOISK_SEARCH_API, params=payload).json()
     rating = response['searchFilms'][0].get('rating', None)
     try:
         rating = float(rating[0:3])
-    except:
+    except ValueError:
         if rating is not None:
             rating = float(rating[:2]) / 10
         else:
@@ -50,8 +55,9 @@ def fetch_movie_info(movie_title):
 
 
 def sort_movies_by_rating(movies):
-    movies.sort(key=lambda movie: movie['rating'], reverse=True)
-    return movies
+    sorted_movies = movies
+    sorted_movies.sort(key=lambda movie: movie['rating'], reverse=True)
+    return sorted_movies
 
 
 def output_movies_to_console(movies, films_number):
@@ -70,24 +76,18 @@ def get_full_info(movies):
     return films
 
 
-def in_lots_of_cinemas(min_cinemas_amount, movies):
-    films = []
-    for movie in movies:
-        if int(movie['cinema_amount']) > min_cinemas_amount:
-            films.append(movie)
+def films_in_lots_of_cinemas(min_cinemas_amount, movies):
+    films = list(filter(lambda film: int(film['cinema_amount']) > min_cinemas_amount, movies))
     return films
-
 
 if __name__ == '__main__':
     min_cinemas = int(input('В скольких кинотеатрах '
                             '(минимум) должен идти фильм? --> '))
     films_needed = int(input('Сколько фильмов вывести? --> '))
-    html_page = fetch_afisha_page()
-    html_page = 'page.html'
-    page_div_tags = parse_afisha_list(html_page)
-    os.remove(html_page)
+    html_page_name = fetch_afisha_page()
+    page_div_tags = parse_afisha_list(html_page_name)
     afisha_movies = get_movies_from_afisha(page_div_tags)
     movies = get_full_info(afisha_movies)
-    films = sort_movies_by_rating(movies)
-    movies = in_lots_of_cinemas(min_cinemas, films)
-    output_movies_to_console(movies, films_needed)
+    films_in_many_cinemas = sort_movies_by_rating(movies)
+    movies_in_cinemas = films_in_lots_of_cinemas(min_cinemas, films_in_many_cinemas)
+    output_movies_to_console(movies_in_cinemas, films_needed)
